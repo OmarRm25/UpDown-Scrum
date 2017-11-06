92ef173bd3eae2b577c026e28a900acdab7614dd
jeshc <hcjesus@gmail.com>
2017-11-06 15:57:13 -0600

    Se crea Controlador de autocuidado y la accion de aseo

---
+++ application/controllers/Autocuidado.php
@@ -0,0 +1,23 @@
+<?php
+
+class Autocuidado extends CI_Controller{
+
+  public function __construct()
+  {
+    parent::__construct();
+    //Codeigniter : Write Less Do More
+    $this->load->helper('url_helper');
+  }
+
+ public function index()
+  {
+    echo 'Hola index';
+  }
+
+  public function aseo(){
+    $this->load->view('templates/header');
+    $this->load->view('autocuidado/aseo.php');
+    $this->load->view('templates/footer');
+  }
+
+}
--- application/views/pages/menu.php
+++ application/views/pages/menu.php
@@ -75,12 +75,12 @@
 <section  >
 
 	<ul id="navs">
-		<li><a class="btn-floating red"><i class="material-icons">insert_chart</i></a></li>
+		<li><a class="btn-floating red"><i class="material-icons"  >insert_chart</i></a></li>
 		<li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a></li>
 		<li><a class="btn-floating green"><i class="material-icons">publish</i></a></li>
 		<li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a></li>
 
-		<li><a class="btn-floating red"><i class="material-icons">insert_chart</i></a></li>
+		<li><a class="btn-floating red" href="./autocuidado/aseo"><i class="material-icons" >insert_chart</i></a></li>
 	</ul>
 
 
@@ -108,7 +108,7 @@
 		<i class="material-icons">menu</i>
 	</a>
 	<ul>
-		<li><a class="btn-floating red"><i class="material-icons">insert_chart</i></a></li>
+		<li><a class="btn-floating red"><i class="material-icons"  >insert_chart</i></a></li>
 		<li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a></li>
 		<li><a class="btn-floating green"><i class="material-icons">publish</i></a></li>
 		<li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a></li>
