078934c866dd14569b161aa9b2abb0b08982a5fd
jeshc <hcjesus@gmail.com>
2017-11-08 23:31:21 -0600

    Implementacion inicial del c{odigo de Ivan para alimentos localhost//index.php/autocuidado/alimentacion

--- application/views/autocuidado/alimentacion.php
+++ application/views/autocuidado/alimentacion.php
@@ -1,15 +1,50 @@
+<body>
 <div class="container">
-  <div class="row" ></div>
 
+  <nav>
+
+    <div class="row card-panel hoverable grey lighten-5  ">
+        <a href="#!" class="col s2 m4 2 black-text text-darken-2 "><i class="material-icons">&#xE5C4;</i> <h6>Pagina Anterior</h6></a>
+        <H5 class="col s2 m6 black-text text-darken-2 left-align hide-on-small-only hide-on-med-only">Conoce los Diferentes alimentos.</H5>
+        <h5 class="col s12 m6 black-text text-darken-2 center-align hide-on-large-only">Conoce los Diferentes alimentos.</h5>
+        <a href="#!" class="col s12 m4 2 black-text text-darken-2 right-align right "><i class="material-icons">&#xE5C8;</i> <h6> Pagina Siguiente</h6></a>
+
+    </div>
+  </nav>
   <div class="row">
-          <div class="col s12 m6">
-            <div class="card green lighten-3 ">
-              <div class="card-content black-text">
-                <span class="card-title"><?php echo  $titulo; ?></span>
-                 <img src="<?php echo base_url();?>assets/media/img/alimentos.jpg" alt="Alimentos" > 
-              </div>
+    <section>
+      <br>
+      <ul class="tabs tablas">
+        <li class="tab col s1"><a href="#grano">GRANOS</a></li>
+        <li class="tab col s1"><a href="#vege">VEGETABLES</a></li>
+        <li class="tab col s1"><a href="#fruta">FRUTAS</a></li>
+        <li class="tab col s1"><a href="#carne">CARNES</a></li>
+        <li class="tab col s1"><a href="#lac">LACTEOS</a></li>
+      </ul>
+      <div id="grano" class="col s12 ">
+        <div class="row">
+                <div class="col s4">
+                  <div class="card">
+                    <div class="card-image materialboxed" width="650">
+                      <img src="<?php echo base_url();?>assets/media/img/alimentos/carnes/CarneRes.jpg" >
+                      <a class="btn-floating halfway-fab waves-effect waves-light red btn modal-trigger" href="#1" onclick="rep('Carne De RES');"><i class="material-icons">&#xE037;</i></a>
+                    </div>
+                    <div class="card-content">
+                      <p>Carne de res</p>
+                    </div>
+                  </div>
 
-            </div>
-          </div>
+                </div>
         </div>
+    </div>
+
+
+      <div id="vege" class="col s12 ">Test 2</div>
+      <div id="fruta" class="col s12 ">Test 3</div>
+      <div id="carne" class="col s12 ">Test 2</div>
+      <div id="lac" class="col s12 ">Test 3</div>
+    </section>
+  </div>
+
+
       </div>
